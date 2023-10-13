18089235faf8b736021a74d774643546ed285348
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-10-13 17:53:31 -0400

    installed neccessary node files into the async folder. Solved all of the problems in the proble set

--- IntermediateJavaScript/Asynchronicity/TImeout Project Phase 1/problems/02-set-seconds-timeout-args.js
+++ IntermediateJavaScript/Asynchronicity/TImeout Project Phase 1/problems/02-set-seconds-timeout-args.js
@@ -24,6 +24,9 @@ setSecondsTimeoutArgs(function(arg1, arg2) {
 
 function setSecondsTimeoutArgs(cb, delayInSeconds, ...args) {
   // Your code here
+  delayInMS = delayInSeconds * 1000;
+
+  setTimeout(cb, delayInMS, ...args);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
