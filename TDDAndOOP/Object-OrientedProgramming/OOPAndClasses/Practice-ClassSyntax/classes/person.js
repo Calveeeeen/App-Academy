118b12baccdb7e5022ad5a5fd328f2b787612cde
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-10-21 19:02:14 -0400

    practice - class syntax

--- TDDAndOOP/Object-OrientedProgramming/OOPAndClasses/Practice-ClassSyntax/classes/person.js
+++ TDDAndOOP/Object-OrientedProgramming/OOPAndClasses/Practice-ClassSyntax/classes/person.js
@@ -1,10 +1,41 @@
 // Your code here
+class Person {
 
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    introduce(){
+        console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`)
+    }
+
+
+
+    static introducePeople(arr){
+        // console.log(Array.isArray(...Person));
+        if(!(Array.isArray(arr))){
+            console.log(`introducePeople only takes an array as an argument.`);
+        }
+        else if(!( instanceof Person)){
+            console.log(`All items in array must be Person class instances`)
+        }
+        else{
+            return people.map((person) => person.introduce());
+        }
+
+    }
+
+}
+
+const person = new Person('Calvin', "Cheung", "26");
+// Person.introducePeople();
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Person;
+    module.exports = Person;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
