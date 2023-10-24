a43e7a84d27a7d1330f289462e90dd528e157739
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-10-24 15:33:05 -0400

    completed Inheritance practice

--- TDDAndOOP/Object-OrientedProgramming/OOPAndClasses/Practice-Inheritance/classes/student.js
+++ TDDAndOOP/Object-OrientedProgramming/OOPAndClasses/Practice-Inheritance/classes/student.js
@@ -1,6 +1,28 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+
+  constructor(firstName, lastName, major, GPA){
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2){
+    if(student1.GPA > student2.GPA){
+      let name = `${student1.firstName} ${student1.lastName}`;
+      return `${name} has the higher GPA.`;
+    }
+    else if (student2.GPA > student1.GPA){
+      let name = `${student2.firstName} ${student2.lastName}`;
+      return `${name} has the higher GPA.`;
+    }
+    else{
+      return "Both students have the same GPA";
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
