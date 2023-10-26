020f20ea5dcbe9251812eb2f3d12195418b7e263
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-10-26 15:59:02 -0400

    completed phase 1 of friendly and evil dragons project

--- TDDAndOOP/Object-OrientedProgramming/BasicProjects/LongPractice-FriendlyAndEvilDragons/classes/dragon.js
+++ TDDAndOOP/Object-OrientedProgramming/BasicProjects/LongPractice-FriendlyAndEvilDragons/classes/dragon.js
@@ -1,10 +1,33 @@
 // Your code here
+class Dragon {
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+    }
+
+    breathesFire(){
+        return `${this.name} breathes fire everywhere! BURN!!!!`;
+    }
+
+    static getDragons(...dragon){
+        let dragons = [];
+        dragon.forEach(function(el){
+            dragons.push(el.name);
+        })
+        return dragons;
+    }
+}
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Dragon;
+    module.exports = Dragon;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
