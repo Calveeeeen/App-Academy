6242240762c6168fb3b53c6bf83751b1b4b1a0fc
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-10-26 18:38:28 -0400

    completed phase 2

--- TDDAndOOP/Object-OrientedProgramming/BasicProjects/LongPractice-FriendlyAndEvilDragons/classes/friendly-dragon.js
+++ TDDAndOOP/Object-OrientedProgramming/BasicProjects/LongPractice-FriendlyAndEvilDragons/classes/friendly-dragon.js
@@ -1,1 +1,42 @@
 // Your code here
+const Dragon = require("./dragon")
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals=[], friend){
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    }
+
+    hasLifeGoals(){
+        let lifeGoals = this.lifeGoals;
+
+        for(let i=0; i<lifeGoals.length; i++){
+            let goal = lifeGoals[i];
+            console.log(`${this.name} likes to ${goal}`);
+        }
+    }
+
+    helpsPeople(){
+        // console.log(`${this.name} helps their friend ${this.friend}`);
+        return `${this.name} helps their friend ${this.friend}`;
+    }
+
+}
+
+const puff = new FriendlyDragon(
+    "Puff",
+    "green",
+    [
+      "live by the sea",
+      "frolick in the autumn mist",
+      "help small children"
+    ],
+    "Jackie Paper"
+  );
+
+  console.log(puff);
+  console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+  puff.hasLifeGoals();
+  console.log(puff.helpsPeople());
+module.exports = FriendlyDragon;
