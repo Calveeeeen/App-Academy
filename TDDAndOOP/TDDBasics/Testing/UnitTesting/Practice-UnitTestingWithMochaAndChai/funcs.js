504fc205d7c8791651a74321d14fc2cfc511b7c9
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-11-02 05:03:57 -0400

    completed writing unit test practice

--- TDDAndOOP/TDDBasics/Testing/UnitTesting/Practice-UnitTestingWithMochaAndChai/funcs.js
+++ TDDAndOOP/TDDBasics/Testing/UnitTesting/Practice-UnitTestingWithMochaAndChai/funcs.js
@@ -1,13 +1,37 @@
 function isFive(num) {
   // Your code here
+  if (num===Number(5)){
+    return true;
+  }
+  return false;
 }
 
 function isOdd(number) {
   // Your code here
+  if(number%2 ==0){
+    return false;;
+  }
+  else if(typeof number !== 'number'){
+    throw new Error("This is not a number");
+  }
+  return true;
+
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  let newArr = [];
+  if(step != null){
+    for(let i=min; i<=max; i+=step){
+      newArr.push(i);
+    }
+  }
+  else{
+    for(let i= min; i<=max; i+= step){
+      newArr.push(i);
+    }
+  }
+  return newArr;
 }
 
 
--- TDDAndOOP/TDDBasics/Testing/UnitTesting/Practice-UnitTestingWithMochaAndChai/test/class-spec.js
+++ TDDAndOOP/TDDBasics/Testing/UnitTesting/Practice-UnitTestingWithMochaAndChai/test/class-spec.js
@@ -5,30 +5,40 @@ const { Word } = require("../class");
 
 describe("Word", function () {
   describe("Word constructor function", function () {
+    const yes = new Word("yes");
+
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(yes).to.have.property('word').that.is.equal('yes');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(yes.word).to.equal('yes');
     });
   });
 
   describe("removeVowels function", function () {
+    const yes = new Word("yes");
+    const result = yes.removeVowels();
+
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      expect(result).to.equal('ys');
     });
   });
 
   describe("removeConsonants function", function () {
+    const example = new Word("example");
+    const result = example.removeConsonants();
+
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      expect(result).to.equal("eae");
     });
   });
-  
+
   describe("pigLatin function", function () {
+    const example = new Word("example");
+    const result = example.pigLatin();
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      expect(result).to.equal('exampleyay');
     });
   });
 });
