d98f1f7d5d7f5bd17ae3bec82e65c2f2d892e70a
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2023-11-09 19:17:10 -0500

    completed TDD-Style assignment

--- TDDAndOOP/TDDBasics/BasicProject/LongPractice-TDDStyle/problems/my-map.js
+++ TDDAndOOP/TDDBasics/BasicProject/LongPractice-TDDStyle/problems/my-map.js
@@ -1,5 +1,16 @@
 function myMap(inputArray, callback) {
   // Your code here
+  let newArr = [];
+  for(let i=0; i<inputArray.length; i++){
+    let el = inputArray[i];
+    let res = callback(el);
+    newArr.push(res);
+  }
+  return newArr;
 }
+
+
+double = (el) => el * 2;
+myMap([1,2,3], double)
 
 module.exports = myMap;
--- TDDAndOOP/TDDBasics/BasicProject/LongPractice-TDDStyle/test/my-map-spec.js
+++ TDDAndOOP/TDDBasics/BasicProject/LongPractice-TDDStyle/test/my-map-spec.js
@@ -1,1 +1,25 @@
 // Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const myMap = require("../problems/my-map");
+
+
+
+describe("myMap()", function(){
+    describe("Create a myMap function that has the follow properties", function(){
+        it("should take in parameter array, which is an array", function(){
+            const inputArray = [1,2,3];
+            myMap(inputArray, double);
+            expect(inputArray).to.be.an('array');
+        })
+        it("should take in a secondary parameter callback that takes in a function", function(){
+            myMap([1,2,3], double);
+            expect(double).to.be.an('function');
+        });
+        it("myMap(inputArray, double) should double the values in the original array", function(){
+            myMap([1,2,3], double);
+            const expected = [2,4,6];
+            expect(myMap([1,2,3], double)).to.deep.equal(expected);
+        })
+    });
+});
