eb0d09f06a187c4fb9555d0d630d65e84b35776b
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-03-29 17:30:20 -0400

    completed error type practice

--- TDDAndOOP/IntermediateTDD/CustomErrorTypes/practice-for-week-04-custom-error-types-main/error-types/maximum-length-exceeded-error.js
+++ TDDAndOOP/IntermediateTDD/CustomErrorTypes/practice-for-week-04-custom-error-types-main/error-types/maximum-length-exceeded-error.js
@@ -1,7 +1,18 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
-
+class MaximumLengthExceededError extends ValidationError{
+  constructor(excessLength){
+    super();
+    this.name = 'MaximumLengthExceededError';
+    if(excessLength){
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    }
+    else{
+      this.message = "Maximum length exceeded";
+    }
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- TDDAndOOP/IntermediateTDD/CustomErrorTypes/practice-for-week-04-custom-error-types-main/error-types/validation-error.js
+++ TDDAndOOP/IntermediateTDD/CustomErrorTypes/practice-for-week-04-custom-error-types-main/error-types/validation-error.js
@@ -1,5 +1,12 @@
 // Your code here
+class ValidationError extends Error{
+  constructor(message){
+    super(message || 'Invalid input');
+    this.name = "ValidationError";
+    Error.captureStackTrace(this, ValidationError)
 
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
