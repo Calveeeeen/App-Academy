555e866bf00ce7b8c7d7f36cedf4c9af3ee03b26
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-04-03 01:01:00 -0400

    completed first timing benchmark practice

--- DataStructuresAndAlgorithms/TimeAndSpaceComplexity/TimingBenchmarks/practice-for-week-05-timing-benchmarks-practice-main/phase-2.js
+++ DataStructuresAndAlgorithms/TimeAndSpaceComplexity/TimingBenchmarks/practice-for-week-05-timing-benchmarks-practice-main/phase-2.js
@@ -3,13 +3,26 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let arr=[];
+  let range = 11*increment;
+  for(let i=increment; i<range; i+=increment){
+    let sum = addNums(i);
+    arr.push(sum);
+  }
+  console.log(arr);
+  return arr;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
-
+  const results = [];
+  let value = 0;
+  for (let i = 0; i < 10; i++) {
+      value += increment;
+      results.push(addManyNums(value));
+  }
+  return results;
 }
 
 module.exports = [addNums10, addManyNums10];
--- DataStructuresAndAlgorithms/TimeAndSpaceComplexity/TimingBenchmarks/practice-for-week-05-timing-benchmarks-practice-main/phase-3.js
+++ DataStructuresAndAlgorithms/TimeAndSpaceComplexity/TimingBenchmarks/practice-for-week-05-timing-benchmarks-practice-main/phase-3.js
@@ -2,18 +2,42 @@ const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
   // Copy your `addNums10` code here
+  function addNums10(increment) {
+    // Fill this in
+    let arr=[];
+    let range = 11*increment;
+    for(let i=increment; i<range; i+=increment){
+      let sum = addNums(i);
+      arr.push(sum);
+    }
+
+    return arr;
+  }
+  console.time("timer1");
   // Then, add timing code
-
+  console.timeLog("timer1");
   // Your code here
-
+  console.timeEnd("timer1");
 }
 
 
 function addManyNums10Timing(increment) {
 // Copy your `addManyNums10` code here
-// Then, add timing code
-
+  function addManyNums10(increment) {
+  // Fill this in
+    const results = [];
+    let value = 0;
+    for (let i = 0; i < 10; i++) {
+        value += increment;
+        results.push(addManyNums(value));
+    }
+    return results;
+  }
+  console.time("timer2");
+  // Then, add timing code
+  console.timeLog("timer2");
   // Your code here
+  console.timeEnd("timer2");
 
 }
 
