5adfe0a6c86240276c74c26e2094f8b81d4d3632
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-04-11 15:38:42 -0400

    completed dynamic array practice

--- DataStructuresAndAlgorithms/DataStructures1/ArraysAndStacks/practice-for-week-05-dynamic-arrays-practice-main/solution/dynamic-array.js
+++ DataStructuresAndAlgorithms/DataStructures1/ArraysAndStacks/practice-for-week-05-dynamic-arrays-practice-main/solution/dynamic-array.js
@@ -1,19 +1,27 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
+    this.defaultSize = defaultSize;
     // Your code here
-
+    this.data = new Array(defaultSize);
+    this.capacity = defaultSize;
+    this.length = 0;
   }
 
   read(index) {
-
     // Your code here
+    // console.log(this.data);
+    return this.data[index];
   }
 
   unshift(val) {
-
     // Your code here
+    // Shift elements to the right to make space for the new value
+    for(let i=this.length; i>0; i--){
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
 }
