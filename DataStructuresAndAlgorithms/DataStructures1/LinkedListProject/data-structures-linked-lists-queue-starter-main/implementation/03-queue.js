0e4df0325ba11c722b6534b8e4ea8d6198b6d204
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-04-19 01:25:52 -0400

    completed queue test cases

--- DataStructuresAndAlgorithms/DataStructures1/LinkedListProject/data-structures-linked-lists-queue-starter-main/implementation/03-queue.js
+++ DataStructuresAndAlgorithms/DataStructures1/LinkedListProject/data-structures-linked-lists-queue-starter-main/implementation/03-queue.js
@@ -5,17 +5,42 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this.length;
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+        let oldHead = this.head;
+        if(!this.head){
+            return null;
+        }
+        else if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }
+        else{
+            this.head = this.head.next;
+            console.log(oldHead.value);
+            this.length--;
+        }
+        return oldHead.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
