b748b6774650923a679f657d09d928278e31dda9
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-06-21 02:09:25 -0400

    completed hashtables and sets project

--- DataStructuresAndAlgorithms/DataStructures2/HashTablesProject/practice-for-week-05-hash-tables-long-practice-main/phases/01-implementation.js
+++ DataStructuresAndAlgorithms/DataStructures2/HashTablesProject/practice-for-week-05-hash-tables-long-practice-main/phases/01-implementation.js
@@ -8,9 +8,13 @@ class KeyValuePair {
 
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
-  constructor(numBuckets = 8) {
+  constructor(capacity = 2, numBuckets = 8, count =0, data) {
     // Initialize your buckets here
     // Your code here
+    this.numBuckets = numBuckets;
+    this.capacity = capacity;
+    this.count = count;
+    this.data = new Array(this.capacity).fill(null);
   }
 
   hash(key) {
@@ -31,21 +35,96 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
+    if(this.count/this.capacity >= 0.7){
+      this.resize();
+    }
+    let index = this.hashMod(key);
+    let newData = new KeyValuePair(key, value);
+
+    if(this.data[index]== null){
+      this.data[index] = newData;
+    }
+
+    else{
+
+      let pos = this.data[index];
+
+      while(pos !== null){
+
+        if(pos.key == newData.key){
+          pos.value = value;
+          return;
+        }
+
+        if(pos.next == null){
+          break;
+        }
+
+        pos = pos.next;
+      }
+
+      newData.next = this.data[index];
+      this.data[index] = newData;
+    }
+    this.count++;
   }
 
 
   read(key) {
     // Your code here
+    let index = this.hashMod(key);
+    let current = this.data[index];
+    while(current){
+      if(current.key === key){
+        return current.value;
+      }
+      current = current.next;
+    }
+    return undefined;
   }
 
 
   resize() {
     // Your code here
+    let oldData = this.data;
+    this.capacity*=2;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
+
+    for(let bucket of oldData){
+      let current = bucket;
+      while(current){
+        this.insert(current.key, current.value);
+        current = current.next;
+      }
+    }
   }
 
 
   delete(key) {
-    // Your code here
+    let index = this.hashMod(key);
+    let pos = this.data[index];
+    let prev = null;
+
+    while(pos !== null){
+
+      if(pos.key === key){
+        if(prev === null){
+          this.data[index] = pos.next;
+        }
+        else{
+          prev.next = pos.next;
+        }
+
+        this.count--;
+        return;
+      }
+
+      prev = pos;
+      pos = pos.next;
+    }
+
+    return "Key not found";
   }
 }
 
--- DataStructuresAndAlgorithms/DataStructures2/HashTablesProject/practice-for-week-05-hash-tables-long-practice-main/phases/02-practice-problems.js
+++ DataStructuresAndAlgorithms/DataStructures2/HashTablesProject/practice-for-week-05-hash-tables-long-practice-main/phases/02-practice-problems.js
@@ -1,26 +1,151 @@
 function anagrams(str1, str2) {
   // Your code here
+  let obj = {};
+
+  if (str1.length == str2.length) {
+    let letters1 = str1.split("");
+    let letters2 = str2.split("");
+
+    for (let i = 0; i < letters1.length; i++) {
+      let key1 = letters1[i]
+      let key2 = letters2[i];
+      if (key1 in obj) {
+        obj[key1]++;
+      }
+      else {
+        obj[key1] = 1;
+      }
+      if (key2 in obj) {
+        obj[key2]++;
+      }
+      else {
+        obj[key2] = 1;
+      }
+    }
+
+    for (key in obj) {
+      if (obj[key] % 2 == 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 
 function commonElements(arr1, arr2) {
   // Your code here
+  let obj = {};
+  let newArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    let el1 = arr1[i];
+
+    if (el1 in obj) {
+      obj[el1]++;
+    }
+    else {
+      obj[el1] = 1;
+    }
+  }
+
+  for (let t = 0; t < arr2.length; t++) {
+    let el2 = arr2[t];
+
+    if (el2 in obj) {
+      obj[el2]++;
+    }
+    else {
+      obj[el2] = 1;
+    }
+  }
+
+  for (let el in obj) {
+    if (obj[el] > 1) {
+      newArr.push(Number(el));
+    }
+  }
+  return newArr;
 }
 
 
 function duplicate(arr) {
   // Your code here
+  let obj = {};
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let key = arr[i];
+    if (key in obj) {
+      obj[key]++;
+    }
+    else {
+      obj[key] = 1;
+    }
+  }
+  for (key in obj) {
+    if (obj[key] == 2) {
+      return Number(key);
+    }
+  }
 }
 
 
 function twoSum(nums, target) {
   // Your code here
+  let obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    let key = target - num;
+
+    if (key in obj) {
+      return true;
+    }
+    obj[num] = true;
+  }
+  return false;
 }
 
 
 function wordPattern(pattern, strings) {
   // Your code here
+  if (strings.length !== pattern.length) {
+    return false;
+  }
+
+  let charToStr = {};
+  let strToChar = {};
+
+  for (let i = 0; i < strings.length; i++) {
+    let word = strings[i];
+    let char = pattern[i];
+
+    if (charToStr[char] !== undefined) {
+      if (charToStr[char] !== word) {
+        return false;
+      }
+    }
+    else {
+      charToStr[char] = word;
+    }
+
+    if (strToChar[word] !== undefined) {
+      if (strToChar[word] !== char) {
+        return false;
+      }
+    }
+    else {
+      strToChar[word] = char;
+    }
+  }
+  return true;
 }
+
+
 
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
