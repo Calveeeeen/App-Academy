1f025809e724fb771d31be44e147848ab15f66f6
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-08-28 22:33:07 -0400

    completed some test cases on the social grapghs practice 1

--- DataStructuresAndAlgorithms/Graphs/AdjacencyLists/SocialGraphPractice1/practice-for-week-06-social-graph-practice-main/social.js
+++ DataStructuresAndAlgorithms/Graphs/AdjacencyLists/SocialGraphPractice1/practice-for-week-06-social-graph-practice-main/social.js
@@ -9,22 +9,57 @@ class SocialNetwork {
 
   addUser(name) {
     // Your code here
+    this.currentID += 1;
+    let ID = this.currentID;
+
+    let user = {
+      id: ID,
+      name: name
+    };
+
+    this.users[ID] = user;
+
+    this.follows[ID] = new Set();
+
+    return ID;
   }
 
   getUser(userID) {
     // Your code here
+    if(this.users.hasOwnProperty(userID)){
+      return this.users[userID];
+    }
+    return null;
   }
 
   follow(userID1, userID2) {
     // Your code here
+    let followed = false;
+    if(this.users.hasOwnProperty(userID1) && this.users.hasOwnProperty(userID2) && userID1 !== userID2){
+      this.follows[userID1].add(userID2);
+      followed = true;
+      return followed;
+    }
+    else{
+      return followed;
+    }
+
   }
 
   getFollows(userID) {
     // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
     // Your code here
+    let followers = [];
+    while(this.users[this.ID]){
+      if(userID === this.follows[this.ID]){
+        followers.push(this.ID);
+      }
+    }
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
