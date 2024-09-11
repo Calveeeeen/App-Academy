73f08ce78f5f46c6c9f682bfe249f41b4fe905e0
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2024-09-11 19:36:18 -0400

    practicing booleans in python

---
+++ Python/DataTypes/Boolean/UsingNotOr.py
@@ -0,0 +1,11 @@
+# Write a function using the not and or operator that returns the Boolean result of the not and or operator being applied to the function's arguments.
+
+# Write your function, here.
+def not_or(val1, val2):
+    return not val1 or val2
+
+
+print(not_or(True, False))    #> False
+print(not_or(True, True))     #> True
+print(not_or(False, True))    #> True
+print(not_or(False, False))   #> True
---
+++ Python/DataTypes/Boolean/XOR.py
@@ -0,0 +1,15 @@
+# Create a function that returns the xor result of two values.
+
+# Write your function, here.
+def xor(val1, val2):
+   return val1^val2
+
+
+print(xor(False, False))   #>  False
+print(xor(True, False))   #>  True
+print(xor(True, True)) #>  False
+print(xor(5, 3))  #> 6
+print(xor(8, 4))  #> 12
+print(xor(2, 2))  #> 0
+print(xor(1, 2))  #> 3
+print(xor(4, 4))  #> 0
