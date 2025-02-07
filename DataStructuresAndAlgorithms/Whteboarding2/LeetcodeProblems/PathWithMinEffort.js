ceec8b34897eaa01679b15f8c0617a1ae0086d91
Calvin <72275469+Calveeeeen@users.noreply.github.com>
2025-02-06 23:57:04 -0500

    cloned html exercise

--- DataStructuresAndAlgorithms/Whteboarding2/LeetcodeProblems/PathWithMinEffort.js
+++ DataStructuresAndAlgorithms/Whteboarding2/LeetcodeProblems/PathWithMinEffort.js
@@ -2,7 +2,10 @@
 
 // 1631. Path With Minimum Effort
 
-// You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, where heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, (0, 0), and you hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). You can move up, down, left, or right, and you wish to find a route that requires the minimum effort.
+// You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, where
+// heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, (0, 0), and you
+// hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). You can move up, down, left, or
+// right, and you wish to find a route that requires the minimum effort.
 
 // A route's effort is the maximum absolute difference in heights between two consecutive cells of the route.
 
@@ -14,5 +17,40 @@
  * @return {number}
  */
 var minimumEffortPath = function(heights) {
+    // height's is a 2-D array (matrix)
+    let start = heights[0][0];
+    let startPos = [0][0]
+    let row = heights.length-1;
+    let col =  heights[0].length-1;
+    let goal = heights[row][col];
+    let goalPos = [row][col];
+    let min, nextStep;
+    // effort is calculated by largest difference in value
+    // directions of travel up, down, left, right always travel in the direction where the val of next position is
+    // less than the rest
+    let queue = [];
+    let visited = new Set();
 
+    queue.push(startPos);
+    visited.add(`${0},${0}`);
+
+    while(queue.length > 0){
+        let [currRow, currCol] = queue.shift();
+        let directions =[[1,0], [-1,0], [0,1], [0,-1]];
+
+        for (let [rowDir, colDir] of directions){
+            let newRow = currRow + rowDir;
+            let newCol = currCol + colDir;
+            let currPos = heights[currRow][currCol];
+            let newPos = heights[newRow][newCol];
+            if(heights[currRow, currCol] <= heights[newRow][newCol]){
+                min =Math.abs(currPos - newPos)
+                nextStep = `${newRow},${newCol}`
+            }
+            if(newRow >0 && newRow < row && newCol > 0 && newCol < col && !visited.has(`${newRow}, ${newCol}`)){
+                
+            }
+        }
+    }
+    // keep visited to keep track of positions traveled already so you're not going backwards.
 };
